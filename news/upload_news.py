8886de14fe169c217b4c08a484ce30a3736f92af
Aidan White <aidan.white36@gmail.com>
2019-01-19 20:56:44 -0600

    update name for news upload script

--- news/upload_news.py
+++ news/upload_news.py
@@ -12,16 +12,9 @@ connection_string = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(username, pw, host,
 engine = create_engine(connection_string)
 DBSession = sessionmaker(bind=engine)
 
-# model_file_path = "./models/article-classifier_8000x3.pkl"
-# clf = NewsClassifier(model_file=model_file_path)
-# news = get_classified_news(clf)
-# with open('./news.json', 'w') as f:
-#     f.write(json.dumps(news))
-
-news = ""
-with open('./news.json', 'r') as f:
-    news = f.read()
-news = json.loads(news)
+model_file_path = "./models/article-classifier_8000x3.pkl"
+clf = NewsClassifier(model_file=model_file_path)
+news = get_classified_news(clf)
 
 today = datetime.datetime.today().strftime('%Y-%m-%d')
 for topic in news:
