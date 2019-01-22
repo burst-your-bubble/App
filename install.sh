ba5def56f823141ff5ffc9a74f8964c75e622f6a
Tate Bosler <tatebosler@gmail.com>
2019-01-22 11:43:39 -0600

    check in activate_this

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 cd /srv/comps
 
+cp activate_this.py venv/bin/activate_this.py
 npm install
 npm run build
 . ./venv/bin/activate
 pip install -r requirements.txt
 deactivate
 sudo apachectl restart
+sudo service apache2 restart
