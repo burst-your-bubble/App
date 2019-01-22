3d436399c5e26b9be722f5fe3b83d97bea220cca
Tate Bosler <tatebosler@gmail.com>
2019-01-22 12:06:51 -0600

    remove sudo prefixes from script that's being run as sudo

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd /srv/comps
 
-sudo python3 -m virtualenv venv
+python3 -m virtualenv venv
 npm install
 npm run build
 . ./venv/bin/activate
 pip install -r requirements.txt
 deactivate
-sudo apachectl restart
-sudo service apache2 restart
+apachectl restart
+service apache2 restart
