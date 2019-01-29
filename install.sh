0373de34042c47d482595d0c576c2c262ff2ddee
Tate Bosler <tatebosler@gmail.com>
2019-01-29 10:38:51 -0600

    apache restart needs to be done with sudo

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ npm install
 npm run build
 pip3 install -r requirements.txt
 cp /home/ubuntu/news_config.py server/config.py
-apachectl restart
-service apache2 restart
+sudo apachectl restart
+sudo service apache2 restart
