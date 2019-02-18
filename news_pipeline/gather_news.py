23cc5f53decf46dc864d585aeaff6fb34fdb6635
Aidan White <aidan.white36@gmail.com>
2019-02-17 20:21:07 -0600

    More article quality and releance improvements

--- news_pipeline/gather_news.py
+++ news_pipeline/gather_news.py
@@ -14,11 +14,12 @@ from nltk.corpus import stopwords
 from nltk import word_tokenize, pos_tag
 from news_pipeline.sources import sources
 
-HEADLINE_SIMILARITY_CUTOFF = 0.25
+ARTICLE_CHAR_MIN = 500
 NUM_LEFT_ARTICLES = 3
 NUM_RIGHT_ARTICLES = 3
 NUM_CENTER_ARTICLES = 2
 MAX_NUM_TOPICS = 10
+
 
 def get_topics_r_usnews():
     ''' Get top weekly stories from r/USNEWS '''
@@ -29,6 +30,7 @@ def get_topics_r_usnews():
     topics = [article['data']['title'] for article in articles]
     return topics
 
+
 def get_topics_r_politics():
     ''' Get top daily stories from r/POLITICS '''
 
@@ -37,6 +39,7 @@ def get_topics_r_politics():
     articles = json.loads(res.text)['data']['children']
     topics = [article['data']['title'] for article in articles]
     return topics
+
 
 def get_topics_politico():
     ''' Get topc stories from Politico (via newsapi.org) '''
@@ -66,46 +69,44 @@ def get_articles_for_topic(topic):
     except KeyError as ex:
         print(ex)
         return []
-    return data['articles']
+    articles = data['articles']
 
+    # If an article fails to download this keeps it in this list, just without the added keys
+    for i in range(len(articles)):
+        article = articles[i]
+        try:
+            a = newspaper.Article(article['url'])
+            a.download()
+            a.parse()
+            article_text = a.text
+            now = datetime.datetime.today().strftime('%Y-%m-%d')
+            articles[i] = dict(article, text=article_text, dateScraped=now)
+        except:
+            print("Article {0} failed to download".format(article['url']))
+            articles[i] = None
+    articles = [a for a in articles if a is not None]
+    return articles
 
+    
 def classify_article(article, clf):
     ''' Return article object with a L/C/R stance classification '''
 
-    try:
-        a = newspaper.Article(article['url'])
-        a.download()
-        a.parse()
-    except:
-        print("Article {0} failed to download".format(article['url']))
-        return
-
-    article_text = a.text
-
-    stance = clf.predict(article_text)
-
-    classified = {
-        'title': article['title'],
-        'author': article['author'],
-        'source': article['source']['name'],
-        'summary':article['description'],
-        'text': article_text,
-        'stance': stance,
-        'url': article['url'],
-        'imageUrl': article['urlToImage'],
-        'datePublished': article['publishedAt'],
-        'dateScraped': datetime.datetime.today().strftime('%Y-%m-%d')
-    }
-
+    stance = clf.predict(article['text'])
+    classified = dict(article, stance=stance)
     return classified
 
 
 def filter_articles(articles, topic):
-    ''' Return only the articles relevant to the given topic '''
+    ''' Return only the articles that are relevant to the given topic and meet the minimum character limit '''
 
     relevant = lambda a: compare_strings(topic, a['title'])
-            
-    articles = [a for a in articles if relevant(a)]
+    articles = [a for a in articles if (a['title'] is not None and a['text'] is not None)]       
+    articles = [a for a in articles if (relevant(a) and len(a['text']) >= ARTICLE_CHAR_MIN)]
+    return articles
+
+
+def group_articles(articles):
+    ''' Group the articles by their policical stance '''
 
     e = Enumerable(articles)
     left = e.where(lambda a: a['stance'] == 'L').to_list()
@@ -113,8 +114,8 @@ def filter_articles(articles, topic):
     right = e.where(lambda a: a['stance'] == 'R').to_list()
 
     articles = left[:NUM_LEFT_ARTICLES] + center[:NUM_CENTER_ARTICLES] + right[:NUM_RIGHT_ARTICLES]
-    print("Topic: {0} returns {1} articles".format(topic, len(articles)))
     return articles
+
 
 def compare_strings(s1, s2):
     ''' Return true if the two strings share nouns '''
@@ -148,7 +149,10 @@ def get_classified_news(clf, src="r/politics"):
 
     for topic in topics:
         articles = get_articles_for_topic(topic)
+        if not enough_articles(articles):
+            continue
 
+        articles = filter_articles(articles, topic)
         if not enough_articles(articles):
             continue
 
@@ -159,7 +163,8 @@ def get_classified_news(clf, src="r/politics"):
                 continue
             classified_articles.append(ca)
 
-        articles = filter_articles(classified_articles, topic)
+        articles = group_articles(classified_articles)
+        print("Topic: {0} returned {1} articles".format(topic, len(articles)))
         if not enough_articles(articles):
             continue
 
@@ -168,4 +173,4 @@ def get_classified_news(clf, src="r/politics"):
             'articles': articles
         })
 
-    return classified_news[:10]
+    return classified_news[:MAX_NUM_TOPICS]
