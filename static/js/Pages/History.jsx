2a452eeb2d59181b3ffa67bd2355cb2b3ebbda2f
Aidan White <aidan.white36@gmail.com>
2019-02-26 00:10:44 -0600

    Merge with Ritviks' changes

--- static/js/Pages/History.jsx
+++ static/js/Pages/History.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { PageHeader, Panel, ProgressBar } from 'react-bootstrap';
 import { Loading } from '../Components/Loading';
 import { ArticleBox } from '../Components/ArticleBox';
-import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, RadialChart } from 'react-vis';
+import { DiscreteColorLegend, RadialChart } from 'react-vis';
 import { ScoreGraph } from '../Components/ScoreGraph';
 
+const ITEMS = [
+    { title: 'Agree', color: "#5cb85c" },
+    { title: 'Neutral', color: "#ec971f" },
+    { title: 'Disagree', color: "#d9534f" },
+];
 
 export class History extends React.Component {
 
@@ -34,7 +39,7 @@ export class History extends React.Component {
     getProportions(stance) {
         const data = [{
             angle: 0,
-            label: "Disagree",
+            label: "Agree",
             color: "#5cb85c"
         },
         {
@@ -44,7 +49,7 @@ export class History extends React.Component {
         },
         {
             angle: 0,
-            label: "Agree",
+            label: "Disagree",
             color: "#d9534f"
         }];
 
@@ -53,31 +58,31 @@ export class History extends React.Component {
                 case 'left':
                     if (this.state.history[i].stance == 'L') {
                         if (this.state.history[i].response == -1)
-                            data[0].angle++;
+                            data[2].angle++;
                         if (this.state.history[i].response == 0)
                             data[1].angle++;
                         if (this.state.history[i].response == 1)
-                            data[2].angle++;
+                            data[0].angle++;
                     }
                     break;
                 case 'center':
                     if (this.state.history[i].stance == 'C') {
                         if (this.state.history[i].response == -1)
-                            data[0].angle++;
+                            data[2].angle++;
                         if (this.state.history[i].response == 0)
                             data[1].angle++;
                         if (this.state.history[i].response == 1)
-                            data[2].angle++;
+                            data[0].angle++;
                     }
                     break;
                 case 'right':
                     if (this.state.history[i].stance == 'R') {
                         if (this.state.history[i].response == -1)
-                            data[0].angle++;
+                            data[2].angle++;
                         if (this.state.history[i].response == 0)
                             data[1].angle++;
                         if (this.state.history[i].response == 1)
-                            data[2].angle++;
+                            data[0].angle++;
                     }
                     break;
             }
@@ -113,36 +118,46 @@ export class History extends React.Component {
                 <Panel.Body>
                     <h4><b>Opinion Trends</b></h4>
                     <div className="charts">
-                        <RadialChart
-                            data={leftData}
-                            innerRadius={100}
-                            radius={140}
-                            width={300}
-                            height={300}
-                            colorType="literal"
-                            padAngle={0.04}
+                        <section className="centerObjects">
+                            <RadialChart
+                                data={leftData}
+                                innerRadius={100}
+                                radius={140}
+                                width={300}
+                                height={300}
+                                colorType="literal"
+                                padAngle={0.04}
                             //showLabels={true}
-                        />
-                        <RadialChart
-                            data={rightData}
-                            innerRadius={100}
-                            radius={140}
-                            width={300}
-                            height={300}
-                            colorType="literal"
-                            padAngle={0.04}
+                            />
+                            Left
+                        </section>
+                        <section className="centerObjects">
+                            <RadialChart
+                                data={centerData}
+                                innerRadius={100}
+                                radius={140}
+                                width={300}
+                                height={300}
+                                colorType="literal"
+                                padAngle={0.04}
                             //showLabels={true}
-                        />
-                        <RadialChart
-                            data={centerData}
-                            innerRadius={100}
-                            radius={140}
-                            width={300}
-                            height={300}
-                            colorType="literal"
-                            padAngle={0.04}
+                            />
+                            Center
+                        </section>
+                        <section className="centerObjects">
+                            <RadialChart
+                                data={rightData}
+                                innerRadius={100}
+                                radius={140}
+                                width={300}
+                                height={300}
+                                colorType="literal"
+                                padAngle={0.04}
                             //showLabels={true}
-                        />
+                            />
+                            Right
+                        </section>
+                        <DiscreteColorLegend orientation="vertical" width={300} items={ITEMS} />
                     </div>
                     <h4><b>Reading History</b></h4>
                     {articles}
