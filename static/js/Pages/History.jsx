e906928a786518506fd616df685900ea484dbf42
Aidan White <aidan.white36@gmail.com>
2019-02-27 16:29:56 -0600

    Implement pagination for reading history

--- static/js/Pages/History.jsx
+++ static/js/Pages/History.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PageHeader, Panel, Grid, Row, Col } from 'react-bootstrap';
 import { Loading } from '../Components/Loading';
-import { ArticleBox } from '../Components/ArticleBox';
+import { ReadingHistory } from '../Components/ReadingHistory';
 import { ScoreGraph } from '../Components/ScoreGraph';
 import { OpinionChart } from '../Components/OpinionChart';
 
@@ -39,17 +39,7 @@ export class History extends React.Component {
     render() {
         if (this.state.loading) return <Loading />;
 
-        var articles = this.state.history.map(article => {
-            return (
-                <ArticleBox title={article.title}
-                    summary={article.summary}
-                    stance={article.stance}
-                    url={article.url}
-                    id={article.id}
-                    read={article.read}
-                    response={article.response}
-                />)
-        });
+        var articles = this.state.history;
 
         var left = 0, right = 0, center = 0;
 
@@ -94,18 +84,7 @@ export class History extends React.Component {
                     </Row>
                 </Grid>
                 <Panel.Body>
-                    <Panel id="story-box" expanded={this.state.expanded} >
-                        <Panel.Heading style={{ cursor: "pointer" }} onClick={this.handleClick} className="topic-box">
-                            <Panel.Title style={{ fontFamily: 'Avenir Next-DemiBold' }} componentClass="span">
-                                Reading History ({articles.length})
-                            </Panel.Title>
-                        </Panel.Heading>
-                        <Panel.Collapse>
-                            <Panel.Body>
-                                {articles}
-                            </Panel.Body>
-                        </Panel.Collapse>
-                    </Panel>
+                    <ReadingHistory history={this.state.history} />
                 </Panel.Body>
             </div>
         );
